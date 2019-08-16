20da4d58ae4c94328e99b85aa0f8e0fb3f182484
Serhii Minin <minin.serhii@gmail.com>
2019-08-16 11:24:43 +0300

    change site icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `CV | Serhii Minin`,
+        short_name: `cv`,
         start_url: `/`,
+        lang: `en`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
