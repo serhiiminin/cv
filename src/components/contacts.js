47e668281837fefe57f979288bb0faa5002a92a2
Serhii Minin <minin.serhii@gmail.com>
2019-08-20 14:35:59 +0300

    align text of summary

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { MdPrint } from "react-icons/md"
+import { FaTwitter, FaGithub, FaLinkedin, FaTelegram } from "react-icons/fa"
 import styled from "styled-components"
 import Image from "./image"
 
@@ -18,10 +19,33 @@ const ImageWrapper = styled.div`
   overflow: hidden;
 `
 
+const SocialWrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;  
+`
+
+const SocialLink = styled.a`
+  text-decoration: none;
+  color: gray;
+  font-size: 26px;
+  margin-right: 10px;
+`
+
+const PrintIcon = styled(props => <MdPrint {...props} />)`
+  font-size: 26px;
+  color: gray;
+  &:hover {
+    cursor: pointer;
+  }
+  @media print {
+    display: none;
+  }
+`
+
 const Contacts = ({ contacts }) => {
   const { name, position, location, phone, email, linkedin, github, twitter, telegram } = contacts
   const onPrint = () => {
-    window.print();
+    window.print()
   }
 
   return (
@@ -32,14 +56,16 @@ const Contacts = ({ contacts }) => {
       <div>
         <h1>{name}</h1>
         <h2>{position}</h2>
-        <MdPrint onClick={onPrint}/>
         <div>Location: {location}</div>
-        <div>Phone: {phone}</div>
-        <div>Email: {email}</div>
-        <div>Linkedin: {linkedin}</div>
-        <div>Github: {github}</div>
-        <div>Twitter: {twitter}</div>
-        <div>Telegram: {telegram}</div>
+        <div>Phone: {<a href={`tel:${phone}?call`}>{phone}</a>}</div>
+        <div>Email: {<a href={`mailto:${email}`}>{email}</a>}</div>
+        <SocialWrapper>
+          <SocialLink href={linkedin} target='_blank'><FaLinkedin/></SocialLink>
+          <SocialLink href={github} target='_blank'><FaGithub/></SocialLink>
+          <SocialLink href={twitter} target='_blank'><FaTwitter/></SocialLink>
+          <SocialLink href={telegram} target='_blank'><FaTelegram/></SocialLink>
+        </SocialWrapper>
+        <PrintIcon onClick={onPrint}/>
       </div>
     </Section>
   )
@@ -55,7 +81,7 @@ Contacts.propTypes = {
     linkedin: PropTypes.string.isRequired,
     github: PropTypes.string.isRequired,
     twitter: PropTypes.string.isRequired,
-    telegram: PropTypes.string.isRequired
+    telegram: PropTypes.string.isRequired,
   }),
 }
 
--- src/components/summary.js
+++ src/components/summary.js
@@ -2,13 +2,18 @@ import React from "react"
 import PropTypes from "prop-types"
 import { MdAccountCircle } from "react-icons/md"
 import Section from "./section"
+import styled from 'styled-components';
+
+const SummaryText = styled.p`
+  text-align: justify;
+`;
 
 const Summary = ({ summary }) => (
   <Section
     title="Summary"
     icon={<MdAccountCircle/>}
   >
-    {summary}
+    <SummaryText>{summary}</SummaryText>
   </Section>
 )
 
