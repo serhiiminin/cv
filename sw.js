c42db267d29d6e7e72137bc206bd6c36275f9280
Serhii Minin <minin.serhii@gmail.com>
2019-08-23 10:48:00 +0300

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7c17da967d38ed960df3.js"
+    "url": "webpack-runtime-3bd93907f6e4edc9a57e.js"
   },
   {
     "url": "styles.edd4bbc0ca53378c9aba.css"
@@ -42,11 +42,11 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ce7f85ea2d44a866aed4deebabb4a163"
+    "revision": "1efaceae27e807676407fb38fb5e005b"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "d848b7eb571ef9f48521965601a81804"
+    "revision": "2b5589caf121cc1b7eeedb45a11302f7"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
